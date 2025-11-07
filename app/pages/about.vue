cae515f21a23528a5dd760fcdb654fde5ae72566
uuuusssseeeerrrr <kyozzang20@gmail.com>
2025-11-07 12:41:54 +0900

    etc

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,7 +1,6 @@
 <template>
   <section>
     만든목적 : ETF 종목별 가격을 편하게 보기 위해 <br/><br/>
-    source : https://github.com/uuuusssseeeerrrr/etftracker <br/><br/>
     API 사용 : 한국투자증권 API, APEXCHARTS, WISE API(rate)<br/><br/>
     참고사항 : 주식 특징상 실시간 정보와 상이할수 있습니다.<br/><br/>
     서버 과부하 방지를 위해 장 오픈시간에 15분 간격으로 데이터를 가져옵니다.<br/><br/>
